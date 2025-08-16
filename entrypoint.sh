2a6c431350b590030702789c8d63504afcaa28cd
IvanTopGaming <85171599+IvanTopGaming@users.noreply.github.com>
2025-08-16 18:47:29 +0300

    Added env to docker-compose.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ MT_PROXY_SECRET=$(head -c 16 /dev/urandom | xxd -ps)
 echo "Starting MTProxy with secret: $MT_PROXY_SECRET"
 exec ./mtproto-proxy \
     -u nobody \
-    -p 8888 \
-    -H 443 \
+    -p ${MT_PROXY_STAT} \
+    -H ${MT_PROXY_PORT} \
     -S "$MT_PROXY_SECRET" \
     --aes-pwd proxy-secret proxy-multi.conf \
     -M 1
